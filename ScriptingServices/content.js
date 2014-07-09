0ea4cd664e5b604fc4e2159b0a32694ec419950f
nedelcho-delchev-tues <nedelcho_delchev@abv.bg>
2014-07-09 17:35:53 +0300

    Creating and viewing data

---
+++ ScriptingServices/content.js
@@ -0,0 +1,3 @@
+var entityContent_table = require('wiki_sample/content');
+
+entityContent_table.processContent_table();
---
+++ ScriptingServices/users.js
@@ -0,0 +1,3 @@
+var entityUsers_table = require('wiki_sample/users');
+
+entityUsers_table.processUsers_table();
